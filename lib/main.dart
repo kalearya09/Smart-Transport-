bf0b381450def963eb678f42477e214f8cb0e526
AyushKale <ayush.kale24@vit.edu>
2026-04-28 19:42:05 +0530

    Integrate Firebase and add login functionality
    Updated main.dart to integrate Firebase and handle user authentication.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'login_screen.dart';
 import 'map_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,9 +18,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MapScreen(),
+      // Auto-login if user already logged in
+      home: FirebaseAuth.instance.currentUser != null
+          ? const MapScreen()
+          : const LoginScreen(),
     );
   }
 }
